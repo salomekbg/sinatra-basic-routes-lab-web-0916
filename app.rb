3528b350fa4953f76c2fca0ec3668116e8c6e792
SluvsP <salomekbg@gmail.com>
2016-10-13 13:46:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Salomé"
+  end
+
+  get '/hometown' do
+    "My hometown is Rockaway Beach"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Life on the Nickel'"
+  end
 end
